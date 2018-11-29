f5ebf458f3ee91561d0f58c5b40c88d821baebb1
Howard M Chen <31971182+hchen88@users.noreply.github.com>
2018-11-29 14:24:16 -0800

    Add files via upload

--- MotelRoom.java
+++ MotelRoom.java
@@ -1,8 +1,27 @@
 package patternsProject;
 
+import java.util.ArrayList;
+
 public abstract class MotelRoom {
 	
-	private String type;
-	private double cost;
-
+	private String type = "";
+	private double cost = 0;
+	private RoachColony roachColony;
+	private ArrayList<Ammenities> ammenities = null;
+	private int roomNumber = null;
+	
+	
+	public MotelRoom(RoachColony roachColony, 
+			String type, ArrayList<Ammenity> ammenities,
+			int roomNumber) {
+		
+		this.roachColony = roachColony;
+		this.ammenities = ammenities; 
+		this.roomNumber = roomNumber;
+		
+	}
+	
+	public double getCost() {
+	}
+	
 }
