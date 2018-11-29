ae26f71cde977cec27c520d72bba8b5ca440bfca
Howard M Chen <31971182+hchen88@users.noreply.github.com>
2018-11-29 14:24:35 -0800

    Add files via upload

--- DeluxeRoom.java
+++ DeluxeRoom.java
@@ -3,4 +3,5 @@ package patternsProject;
 public class DeluxeRoom extends MotelRoom {
 	
 	private static double cost = 75;
+	
 }
--- RegularRoom.java
+++ RegularRoom.java
@@ -4,5 +4,6 @@ public class RegularRoom extends MotelRoom{
 	
 	private static double cost = 50;
 	
+	
 
 }
